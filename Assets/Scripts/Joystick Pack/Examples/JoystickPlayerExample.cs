30397bd9c3dd8cd0fc786e4ebbb5d3764dab0c06
muratcanakar <muratcan168akar@gmail.com>
2020-09-26 02:31:37 +0300

    Kontrol kodlandı.
    Joystick kodlandı geliştiriliyor.

---
+++ Assets/Scripts/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour
+{
+    public float speed;
+    public VariableJoystick variableJoystick;
+    public Rigidbody rb;
+    public float YonZ,YonX;
+    public float JoyStickDegerZ;
+
+
+    
+    public void FixedUpdate()
+    {
+        YonX = variableJoystick.Horizontal;
+        YonZ = variableJoystick.Vertical;
+        if (YonZ<=-0.0001)
+        {
+            JoyStickDegerZ = -1.2f;
+        }
+        else if (YonZ>=0.0001)
+        {
+            JoyStickDegerZ = 1.2f;
+        }
+        else {
+            JoyStickDegerZ = 0;
+
+        }
+        transform.Rotate(transform.rotation.x, YonX*65*Time.deltaTime, transform.rotation.z);
+        transform.Translate(YonX*speed*Time.deltaTime, 0, JoyStickDegerZ* speed * Time.deltaTime);
+    }
+
+}
